3dada3067093693e0277caea535674b780f9197f
ultimania <yng.ultimania@gmail.com>
2018-09-09 03:29:29 +0000

    first commit

--- base.py
+++ base.py
@@ -1,10 +1,13 @@
 # coding: UTF-8
-import urllib2
+import io,sys
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
+
+import urllib.request, urllib.error
 from bs4 import BeautifulSoup
 
 # Access URL
 url = "http://www.nikkei.com"
-html = urllib2.urlopen(url)
+html = urllib.request.urlopen(url)
 soup = BeautifulSoup(html, "html.parser")
 
 # get title
@@ -12,6 +15,7 @@ title_tag = soup.title
 title = title_tag.string
 
 # print
-print title_tag
-print title
+print(title_tag)
+print(title)
+
 

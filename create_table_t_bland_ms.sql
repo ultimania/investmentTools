075e140581311c8fc4fb6716883e0e2d88783bd8
ultimania <yng.ultimania@gmail.com>
2018-09-11 10:51:18 +0900

    fix delete space

--- create_table_t_bland_ms.sql
+++ create_table_t_bland_ms.sql
@@ -4,12 +4,12 @@ CREATE TABLE DB_INVESTMENT.T_BLAND_MS (
     BLAND_CD            INT,                                                    -- 銘柄コード
     BLAND_NAME          VARCHAR(128)    NOT NULL,                               -- 銘柄名
     MARKET_PROD_CLS     VARCHAR(64)     NOT NULL,                               -- 市場・商品区分
-    INDUSTRY_CD         INT                     ,                               -- 業種コード
-    INDUSTRY_NAME       VARCHAR(64)             ,                               -- 業種区分
-    SUB_INDUSTRY_CD     INT                     ,                               -- サブ業種コード
-    SUB_INDUSTRY_NAME   VARCHAR(64)             ,                               -- サブ業種区分
-    SCALE_CD            INT                     ,                               -- 規模コード
-    SCALE_NAME          VARCHAR(64)             ,                               -- 規模区分
+    INDUSTRY_CD         INT,                                                    -- 業種コード
+    INDUSTRY_NAME       VARCHAR(64),                                            -- 業種区分
+    SUB_INDUSTRY_CD     INT,                                                    -- サブ業種コード
+    SUB_INDUSTRY_NAME   VARCHAR(64),                                            -- サブ業種区分
+    SCALE_CD            INT,                                                    -- 規模コード
+    SCALE_NAME          VARCHAR(64),                                            -- 規模区分
     FETCH_FLG           INT NOT NULL,                                           -- 取込フラグ
     PRIMARY KEY(BLAND_ID),
     FOREIGN KEY(MARKET_PROD_CLS)     REFERENCET T_CLS_MS(VALUE),                -- 区分マスタ.値

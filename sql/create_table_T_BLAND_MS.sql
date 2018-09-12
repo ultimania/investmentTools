4f3f5174b04b493e5411a93b33e7992a2d403b2a
ultimania <yng.ultimania@gmail.com>
2018-09-12 09:23:56 +0900

    modified delete name columns

--- sql/create_table_T_BLAND_MS.sql
+++ sql/create_table_T_BLAND_MS.sql
@@ -5,11 +5,8 @@ CREATE TABLE db_investment.T_BLAND_MS (
     BLAND_NAME          VARCHAR(128)    NOT NULL,                               -- 銘柄名
     MARKET_PROD_CLS     VARCHAR(64)     NOT NULL,                               -- 市場・商品区分
     INDUSTRY_CD         INT             NOT NULL,                               -- 業種コード
-    INDUSTRY_NAME       VARCHAR(64)     NOT NULL,                               -- 業種区分
     SUB_INDUSTRY_CD     INT             NOT NULL,                               -- サブ業種コード
-    SUB_INDUSTRY_NAME   VARCHAR(64)     NOT NULL,                               -- サブ業種区分
     SCALE_CD            INT             NOT NULL,                               -- 規模コード
-    SCALE_NAME          VARCHAR(64)     NOT NULL,                               -- 規模区分
     FETCH_FLG           INT             NOT NULL,                               -- 取込フラグ
     PRIMARY KEY(BLAND_CD),
     FOREIGN KEY(INDUSTRY_CD)         REFERENCES T_INDUSTRY_MS(INDUSTRY_CD),     -- 業種マスタ.業種コード

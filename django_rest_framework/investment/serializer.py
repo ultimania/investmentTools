2841956791167892e355cc7ecfdcd66610904c7e
深谷匠 <yng.ultimania@gmail.com>
2018-09-17 16:01:43 +0900

    modified add fields '__all__'

--- django_rest_framework/investment/serializer.py
+++ django_rest_framework/investment/serializer.py
@@ -10,20 +10,25 @@ from .models import T_STK_PRC_TR
 class TargetParamSerializer(serializers.ModelSerializer):
     class Meta:
         model = T_TRG_PRM_MS
+        fields = '__all__' 
 
 class ScaleSerializer(serializers.ModelSerializer):
     class Meta:
         model = T_SCALE_MS
+        fields = '__all__' 
 
 class IndustrySerializer(serializers.ModelSerializer):
     class Meta:
         model = T_INDUSTRY_MS
+        fields = '__all__' 
 
 class BlandSerializer(serializers.ModelSerializer):
     class Meta:
         model = T_BLAND_MS
+        fields = '__all__' 
 
 class StockPriceSerializer(serializers.ModelSerializer):
     class Meta:
         model = T_STK_PRC_TR
+        fields = '__all__' 
 

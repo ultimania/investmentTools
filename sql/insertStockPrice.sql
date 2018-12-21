d41da50dfb635f17cdf2cdc30fc995d5e85dc930
ultimania <yng.ultimania@gmail.com>
2018-12-21 16:29:42 +0900

    create file

---
+++ sql/insertStockPrice.sql
@@ -0,0 +1,21 @@
+insert into {TABLE_NAME} (
+    market_prod_cls ,
+    current_price ,
+    day_before_ratio ,
+    opening_price ,
+    high_orice ,
+    low_price ,
+    sales_volume ,
+    created_at ,
+    bland_cd_id
+) values (
+    {MARKET_PROD_CLS}, 
+    {CURRENT_PRICE},
+    {DAY_BEFORE_RATIO},
+    {OPENING_PRICE},
+    {HIGH_PRICE},
+    {LOW_PRICE},
+    {SALES_VOLUME},
+    {CREATE_TIMESTAMP},
+    {BLAND_CD}
+);

90c810adf463d4ce08f7eae58884521a59576379
ultimania <ultimania@sic.com>
2019-07-11 14:27:35 +0900

    create roorm branch

--- sql/create_table_T_SCALE_MS.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE db_investment.T_SCALE_MS;
-
-CREATE TABLE db_investment.T_SCALE_MS (
-    SCALE_CD            INT,                                                    -- 規模コード
-    SCALE_NAME          VARCHAR(128)    NOT NULL,                               -- 規模名
-    PRIMARY KEY(SCALE_CD)
-) ENGINE=INNODB;

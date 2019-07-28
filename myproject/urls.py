c67e6f47735df89601b80a4e9c4ddd5601944eea
Your Name <you@example.com>
2019-07-28 05:40:27 +0000

    create twitter_api branch

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,8 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mytrade/', include(('mytrade.urls', 'mytrade'),)),
-    path('accounts/', include(('accounts.urls', 'accounts'),)),
-    path('accounts/', include(('django.contrib.auth.urls'),)),
     path('twitter/', include(('feivs2019AccountManager.urls', 'twitter'),)),
 ]
--- mytrade/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('bland/', views.bland, name='bland'),
-    path('login/', views.login, name='login'),
-]
-

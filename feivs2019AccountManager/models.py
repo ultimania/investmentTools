155d19191c4005284df217690a198952bd5254ae
Your Name <you@example.com>
2019-07-28 11:50:16 +0000

    modified org None

--- feivs2019AccountManager/models.py
+++ feivs2019AccountManager/models.py
@@ -285,16 +285,16 @@ class UsersManager(models.Manager):
         for my_user in self.my_users:
             user_model = {} # 各ユーザのデータ格納用
             if my_user in tmp_dict['statics_favorite']:
-                org = users_master.filter(user_id=my_user).values_list('favourites_cnt_for_me',flat=True).get()
-                org = 0 if org is None else org
+                tmp = users_master.filter(user_id=my_user).values_list('favourites_cnt_for_me',flat=True).get()
+                org = 0 if org is None else tmp
                 user_model['favourites_cnt_for_me'] = tmp_dict['statics_favorite'][my_user] + org
             if my_user in tmp_dict['statics_retweet']:
-                org = users_master.filter(user_id=my_user).values_list('retweets_cnt_for_me',flat=True).get()
-                org = 0 if org is None else org
+                tmp = users_master.filter(user_id=my_user).values_list('retweets_cnt_for_me',flat=True).get()
+                org = 0 if org is None else tmp
                 user_model['retweets_cnt_for_me'] = tmp_dict['statics_retweet'][my_user] + org
             if my_user in tmp_dict['statics_reply']:
-                org = users_master.filter(user_id=my_user).values_list('my_replies_cnt',flat=True).get()
-                org = 0 if org is None else org
+                tmp = users_master.filter(user_id=my_user).values_list('my_replies_cnt',flat=True).get()
+                org = 0 if org is None else tmp
                 user_model['my_replies_cnt'] = tmp_dict['statics_reply'][my_user] + org
             user_model_data[my_user] = user_model
 

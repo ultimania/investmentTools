c4d621f1b51d09f434ef17c9c6d4347b2647c6d3
Your Name <you@example.com>
2019-08-03 14:36:35 +0000

    This is auto commit by coverage

--- coverage.sh
+++ coverage.sh
@@ -6,6 +6,7 @@ echo "${REPORT}"
 RESULT=`echo ${REPORT} | sed -e 's/.* //g' | sed -e 's/%//g'`
 COMMENT=${1:-"This is auto commit by coverage"}
 if [ ${RESULT} -gt 80 ]; then
+    git remote set-url origin `git remote -v | awk '{print $2}' | sed -e "s#https://github.com/#git@github.com:#g" | head -1`
     git add -A
     git commit -a -m "${COMMENT}"
     git push

5a891b93f782492cbf69c6bd60ff97d1cb2f03bd
Your Name <you@example.com>
2019-08-03 14:23:59 +0000

    This is auto commit by coverage

--- coverage.sh
+++ coverage.sh
@@ -8,6 +8,7 @@ COMMENT=${1:-"This is auto commit by coverage"}
 if [ ${RESULT} -gt 80 ]; then
     git add -A
     git commit -a -m "${COMMENT}"
+    git push
     exit 0
 else
     "WARN: Coverage report value less than 80%"
